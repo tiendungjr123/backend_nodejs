915c6e99b04127f6b86c4fb3b1a880a661fe17c6
tiendungjr <tiendungjr123@gmail.com>
2025-07-24 18:37:32 +0700

    install expressjs

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const hostname = "localhost"; //cái này === http://localhost
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  //Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World\n Tien dung");
+});
+//listen for request on port 3000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
